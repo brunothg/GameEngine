62cfbb29fbc65c985252f54327530b20d06eb112
brunothg <android.bruns@googlemail.com>
2015-03-07 11:30:54 +0100

    Stage and Scene

---
+++ src/test/java/game/engine/time/TimeUtilsTest.java
@@ -0,0 +1,28 @@
+package game.engine.time;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeUtilsTest {
+
+	@Test
+	public void nanoToSecondTest() throws Exception {
+
+		long nano = 456882345236L;
+		double seconds = TimeUtils.Seconds(nano);
+
+		assertEquals(456, (long) seconds);
+	}
+
+	@Test
+	public void secondInMilli() throws Exception {
+
+		int second = 57568;
+		double milliseconds = TimeUtils.Milliseconds(TimeUtils
+				.NanosecondsOfSeconds(second));
+
+		assertEquals(57568000, (int) milliseconds);
+	}
+
+}
