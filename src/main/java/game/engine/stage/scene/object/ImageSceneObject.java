9a27c81fbd6b910ade8ff167bd983cb1ad094f10
brunothg <android.bruns@googlemail.com>
2015-03-08 10:30:50 +0100

    ImageSceneObject

---
+++ src/main/java/game/engine/stage/scene/object/ImageSceneObject.java
@@ -0,0 +1,73 @@
+package game.engine.stage.scene.object;
+
+import game.engine.image.ImageUtils;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+/**
+ * A {@link SceneObject} that consists of an image. The default origin is set to
+ * Point(0,0). The size is set to the image's size. The image will be scaled to
+ * fit the size of this object.
+ * 
+ * @author Marvin Bruns
+ *
+ */
+public class ImageSceneObject extends SceneObject {
+
+	private BufferedImage image;
+
+	/**
+	 * Create a new {@link SceneObject} that displays an image. The origin is
+	 * set to P(0,0). The size is set to the image's size.
+	 * 
+	 * @param image
+	 *            Shown image
+	 */
+	public ImageSceneObject(BufferedImage image) {
+
+		setImage(image);
+		setSize(image.getWidth(), image.getHeight());
+	}
+
+	/**
+	 * @see #ImageSceneObject(BufferedImage)
+	 */
+	public ImageSceneObject(Image image) {
+
+		this(ImageUtils.BufferedImage(image));
+	}
+
+	@Override
+	protected void paint(Graphics2D g, long elapsedTime) {
+
+		BufferedImage image = getImage();
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), 0, 0, width, height,
+				null);
+	}
+
+	@Override
+	public Point getOrigin() {
+
+		return new Point(0, 0);
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+
+		if (image == null) {
+			throw new IllegalArgumentException("Null value not allowed");
+		}
+
+		this.image = image;
+	}
+
+}
