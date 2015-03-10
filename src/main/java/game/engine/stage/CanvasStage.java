038688164ca403b50ddf7af1281ebebcc848cde3
brunothg <android.bruns@googlemail.com>
2015-03-10 17:28:57 +0100

    uid

--- src/main/java/game/engine/stage/CanvasStage.java
+++ src/main/java/game/engine/stage/CanvasStage.java
@@ -4,26 +4,24 @@ import java.awt.Canvas;
 
 import game.engine.stage.scene.Scene;
 
-public class CanvasStage extends Canvas implements Stage
-{
+public class CanvasStage extends Canvas implements Stage {
+
+	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void tick(long frames, long coveredTime)
-	{
+	public void tick(long frames, long coveredTime) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void setScene(Scene scene)
-	{
+	public void setScene(Scene scene) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public Scene getScene()
-	{
+	public Scene getScene() {
 		// TODO Auto-generated method stub
 		return null;
 	}
