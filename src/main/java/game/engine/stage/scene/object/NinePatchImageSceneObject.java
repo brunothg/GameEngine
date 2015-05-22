5a5cdc8896a28923da32508e3d6de8bd8fefb38e
Marvin Bruns <android.bruns@googlemail.com>
2015-05-22 07:29:20 +0200

    get insets

--- src/main/java/game/engine/stage/scene/object/NinePatchImageSceneObject.java
+++ src/main/java/game/engine/stage/scene/object/NinePatchImageSceneObject.java
@@ -5,48 +5,59 @@ import game.engine.image.NinePatchImage;
 
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.image.BufferedImage;
 
 /**
  * 
- * This class fetches a {@link NinePatchImage} and puts it in the
- * {@link SceneObject} context.
+ * This class fetches a {@link NinePatchImage} and puts it in the {@link SceneObject} context.
  * 
  * @author Marvin Bruns
  *
  */
-public class NinePatchImageSceneObject extends SceneObject {
+public class NinePatchImageSceneObject extends SceneObject
+{
 
 	NinePatchImage image;
 
-	public NinePatchImageSceneObject(BufferedImage image) {
+	public NinePatchImageSceneObject(BufferedImage image)
+	{
 
 		this.image = new NinePatchImage(image);
 	}
 
-	public NinePatchImageSceneObject(NinePatchImage image) {
+	public NinePatchImageSceneObject(NinePatchImage image)
+	{
 
 		this.image = image;
 	}
 
-	public NinePatchImageSceneObject(Image image) {
+	public NinePatchImageSceneObject(Image image)
+	{
 
 		this(ImageUtils.BufferedImage(image));
 	}
 
 	@Override
-	protected void paint(Graphics2D g, long elapsedTime) {
+	protected void paint(Graphics2D g, long elapsedTime)
+	{
 
 		image.draw(g, getWidth(), getHeight());
 	}
 
 	/**
 	 * @see NinePatchImage#setQuality(int)
-	 * @param quality
-	 *            The interpolation quality
+	 * @param quality The interpolation quality
 	 */
-	public void setQuality(int quality) {
+	public void setQuality(int quality)
+	{
 
 		image.setQuality(quality);
+	}
+
+	public Insets getInsets()
+	{
+
+		return image.getInsets();
 	}
 }
