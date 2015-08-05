b989369b2af5cfaebacb342d028a1146a2d3341b
Marvin Bruns <android.bruns@googlemail.com>
2015-08-06 00:31:43 +0200

    ...

--- src/main/java/game/engine/d3/OBJModelParser.java
+++ src/main/java/game/engine/d3/OBJModelParser.java
@@ -3,9 +3,12 @@ package game.engine.d3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import game.engine.utils.ArrayUtils;
 
 /**
  * Read OBJ models
@@ -15,8 +18,11 @@ import org.slf4j.LoggerFactory;
  */
 public class OBJModelParser {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(OBJModelParser.class);
+	private static final char LINE_NEEDS_NEXT_LINE_SIGN = '\\';
+	private static final String COMMENT_REGEX = "^#.*";
+	private static final String ARGUMENT_SPLIT_REGEX = "\\s";
+
+	private static final Logger LOG = LoggerFactory.getLogger(OBJModelParser.class);
 
 	private Reader reader;
 
@@ -32,24 +38,86 @@ public class OBJModelParser {
 	public void parse() throws IOException {
 		BufferedReader in = new BufferedReader(reader);
 
+		String lineBuffer = "";
 		String line;
+		int lineNr = 0;
 		while ((line = in.readLine()) != null) {
 			line = line.trim();
+			lineNr++;
+
 			if (line.isEmpty()) {
+				LOG.warn("Skipp empty line {}", lineNr);
 				continue;
 			}
 
+			if (line.charAt(line.length() - 1) == LINE_NEEDS_NEXT_LINE_SIGN) {
+				LOG.debug("Command needs next line");
+				lineBuffer += line.substring(0, line.length() - 1);
+				continue;
+			}
+			line = lineBuffer + line;
+			lineBuffer = "";
+
+			LOG.debug("Now at line '{}'", lineNr);
 			parseLine(line);
 		}
 
 		in.close();
+		reader = null;
 	}
 
 	private void parseLine(String line) {
 
-		if (line.startsWith("#")) {
-			LOG.debug("Found comment '{}'", line);
+		if (line.matches(COMMENT_REGEX)) {
+			LOG.info("Found comment '{}'", line);
+			return;
 		}
+
+		String[] args = line.split(ARGUMENT_SPLIT_REGEX);
+		parseArguments(args);
+	}
+
+	private void parseArguments(String[] args) {
+		if (args == null || args.length <= 0) {
+			LOG.warn("Skipp empty argument");
+			return;
+		}
+
+		String cmd = args[0].toLowerCase();
+		String[] params = Arrays.copyOfRange(args, 1, args.length);
+
+		switch (cmd) {
+		case "o":
+			createObject(params);
+			break;
+		case "v":
+			createVertex(params);
+			break;
+		case "f":
+			createFace(params);
+			break;
+		default:
+			LOG.warn("Unkown argument '{}'", cmd);
+			break;
+		}
+	}
+
+	private void createFace(String[] params) {
+		int[] vertices = ArrayUtils.toInteger(params);
+		LOG.debug("Create face '{}'", Arrays.toString(vertices));
+		// TODO createFace
+	}
+
+	private void createVertex(String[] params) {
+		double[] position = ArrayUtils.toDouble(params);
+		LOG.debug("Create vertex '{}'", Arrays.toString(position));
+		// TODO createVertex
+	}
+
+	private void createObject(String[] params) {
+		String name = params[0];
+		LOG.info("Create object '{}'", name);
+		// TODO createObject
 	}
 
 }
---
+++ src/main/java/game/engine/utils/ArrayUtils.java
@@ -0,0 +1,25 @@
+package game.engine.utils;
+
+public class ArrayUtils {
+
+	public static double[] toDouble(String[] params) {
+
+		double[] ar = new double[params.length];
+		for (int i = 0; i < params.length; i++) {
+			ar[i] = Double.valueOf(params[i]);
+		}
+
+		return ar;
+	}
+
+	public static int[] toInteger(String[] params) {
+
+		int[] ar = new int[params.length];
+		for (int i = 0; i < params.length; i++) {
+			ar[i] = Integer.valueOf(params[i]);
+		}
+
+		return ar;
+	}
+
+}
