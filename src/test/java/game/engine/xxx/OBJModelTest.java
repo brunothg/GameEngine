39b71364b546396c82afc8839c52f50c8115b4ae
brunothg <android.bruns@googlemail.com>
2015-08-09 19:30:49 +0200

    ...

--- src/test/java/game/engine/xxx/OBJModelTest.java
+++ src/test/java/game/engine/xxx/OBJModelTest.java
@@ -35,7 +35,6 @@ import game.engine.time.FPS;
 import game.engine.time.TimeUtils;
 import game.engine.time.Timer;
 
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -59,8 +58,8 @@ public class OBJModelTest {
 
 	private static void _main(String[] args) throws Exception {
 
-		model = new OBJModelParser(new InputStreamReader(new FileInputStream(
-				"/home/marvin/Downloads/3dtest/skin.obj"),
+		model = new OBJModelParser(new InputStreamReader(
+				OBJModelTest.class.getResourceAsStream("/ape.obj"),
 				StandardCharsets.UTF_8)).parse();
 
 		Display.setDisplayMode(new DisplayMode(800, 600));
