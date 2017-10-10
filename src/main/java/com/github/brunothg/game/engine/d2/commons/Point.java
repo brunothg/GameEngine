5d1f7afc910f7c95fb085652b63dce48fdb0fcf9
Marvin Bruns <android.bruns@googlemail.com>
2017-10-10 15:37:58 +0200

    precise point and size

--- src/main/java/com/github/brunothg/game/engine/d2/commons/Point.java
+++ src/main/java/com/github/brunothg/game/engine/d2/commons/Point.java
@@ -7,64 +7,62 @@ package com.github.brunothg.game.engine.d2.commons;
  * @author Marvin Bruns
  *
  */
-public class Point
-{
+public class Point {
 
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 
-	public Point()
-	{
+	public Point() {
 
 		this(0, 0);
 	}
 
-	public Point(int x, int y)
-	{
+	public Point(double x, double y) {
 		this.setX(x);
 		this.setY(y);
 	}
 
-	public int getX()
-	{
+	public int getX() {
+		return (int) Math.round(x);
+	}
+
+	public double getPreciseX() {
 		return x;
 	}
 
-	public void setX(int x)
-	{
+	public void setX(double x) {
 		this.x = x;
 	}
 
-	public int getY()
-	{
+	public int getY() {
+		return (int) Math.round(y);
+	}
+
+	public double getPreciseY() {
 		return y;
 	}
 
-	public void setY(int y)
-	{
+	public void setY(double y) {
 		this.y = y;
 	}
 
-	public double getVerticalDistanceTo(Point p)
-	{
+	public double getVerticalDistanceTo(Point p) {
 		double vDistance = 0;
 
-		vDistance = Math.abs(getY() - p.getY());
+		vDistance = Math.abs(getPreciseY() - p.getPreciseY());
 
 		return vDistance;
 	}
 
-	public double getHorizontalDistanceTo(Point p)
-	{
+	public double getHorizontalDistanceTo(Point p) {
 		double hDistance = 0;
 
-		hDistance = Math.abs(getX() - p.getX());
+		hDistance = Math.abs(getPreciseX() - p.getPreciseX());
 
 		return hDistance;
 	}
 
-	public double getDistanceTo(Point p)
-	{
+	public double getDistanceTo(Point p) {
 		double distance = 0;
 
 		distance = Math.sqrt(Math.pow(getHorizontalDistanceTo(p), 2) + Math.pow(getVerticalDistanceTo(p), 2));
--- src/main/java/com/github/brunothg/game/engine/d2/commons/Size.java
+++ src/main/java/com/github/brunothg/game/engine/d2/commons/Size.java
@@ -6,43 +6,48 @@ package com.github.brunothg.game.engine.d2.commons;
  * @author Marvin Bruns
  *
  */
-public class Size
-{
+public class Size {
 
-	private int width;
-	private int height;
+	private double width;
+	private double height;
 
-	public Size()
-	{
+	public Size() {
 
 		this(0, 0);
 	}
 
-	public Size(int width, int height)
-	{
+	public Size(double width, double height) {
 
 		this.width = width;
 		this.height = height;
 	}
 
-	public int getWidth()
-	{
+	public int getWidth() {
+		return (int) Math.round(width);
+	}
+
+	public double getPreciseWidth() {
 		return width;
 	}
 
-	public void setWidth(int width)
-	{
+	public void setWidth(double width) {
 		this.width = width;
 	}
 
-	public int getHeight()
-	{
+	public int getHeight() {
+		return (int) Math.round(height);
+	}
+
+	public double getPreciseHeight() {
 		return height;
 	}
 
-	public void setHeight(int height)
-	{
+	public void setHeight(double height) {
 		this.height = height;
+	}
+
+	public double calculateSurface() {
+		return getPreciseWidth() * getPreciseHeight();
 	}
 
 }
