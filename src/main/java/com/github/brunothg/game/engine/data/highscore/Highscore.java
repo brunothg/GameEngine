5f099b0bdbe06a9dd6853ddff07f1b7b3151973e
Marvin Bruns <android.bruns@googlemail.com>
2017-10-11 14:30:20 +0200

    read write highscore

--- src/main/java/com/github/brunothg/game/engine/data/highscore/Highscore.java
+++ src/main/java/com/github/brunothg/game/engine/data/highscore/Highscore.java
@@ -1,6 +1,14 @@
 package com.github.brunothg.game.engine.data.highscore;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -76,4 +84,52 @@ public class Highscore implements Iterable<HighscoreEntry>, Serializable {
 		return scores.iterator();
 	}
 
+	/**
+	 * Write this Highscore to an {@link OutputStream}. Only Name and Score will be
+	 * written.
+	 * 
+	 * @param out
+	 *            The stream for writing
+	 * @throws IOException
+	 */
+	public void store(OutputStream out) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+
+		for (HighscoreEntry entry : this) {
+			writer.write(entry.getPoints() + ";" + entry.getName());
+			writer.newLine();
+		}
+
+		writer.close();
+	}
+
+	/**
+	 * Loads {@link Highscore} from {@link InputStream}. Only Names and Score will
+	 * be loaded.
+	 * 
+	 * @param in
+	 *            The stream for reading
+	 * @throws IOException
+	 */
+	public void load(InputStream in) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+		scores.clear();
+
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.indexOf(';') <= 0) {
+				continue;
+			}
+
+			String pointS = line.substring(0, line.indexOf(';'));
+			String nameS = line.substring(line.indexOf(';') + 1, line.length());
+
+			int score = Integer.valueOf(pointS);
+
+			addScore(new HighscoreEntry(score, nameS));
+		}
+
+		reader.close();
+	}
 }
